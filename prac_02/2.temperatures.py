ce880ee3ddd272c8ee906f3295061f85edfa8b53
asmitasharma01 <asmita.sharma@my.jcu.edu.au>
2024-05-30 09:16:44 +0800

    Added the function for temperature conversion and menu display

--- prac_02/2.temperatures.py
+++ prac_02/2.temperatures.py
@@ -1,7 +1,9 @@
 def celsius_to_fahrenheit(celsius):
+    # Convert Celsius to Fahrenheit
     return celsius * 9/5 + 32
 
 def fahrenheit_to_celsius(fahrenheit):
+    # Convert Fahrenheit to Celsius
     return (fahrenheit - 32) * 5/9
 
 def display_menu():

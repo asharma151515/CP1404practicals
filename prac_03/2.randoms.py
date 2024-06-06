b13ec4b8b5e48ad0798e37c7f34e638c7bff4450
asmitasharma01 <asmita.sharma@my.jcu.edu.au>
2024-06-06 13:13:11 +0800

    this help () fucntion to get information about randint and randrange functions

--- prac_03/2.randoms.py
+++ prac_03/2.randoms.py
@@ -12,8 +12,13 @@ random_number1 = random.randint(5, 20)  # line 1
 random_number2 = random.randrange(3, 10, 2)  # line 2
 random_number3 = random.uniform(2.5, 5.5)  # line 3
 
+# random integer between 5 and 20(inclusive) busing the 'random.randint()'
 print(f"Random number from randint(5, 20): {random_number1}")
+
+# random number between 3 and 10 (Exclusive) stepping by 2 using 'random.randrange()'
 print(f"Random number from randrange(3, 10, 2): {random_number2}")
+
+# random float between 2.5 and 5.5 using 'random.uniform()'
 print(f"Random number from uniform(2.5, 5.5): {random_number3}")
 
 # Generating a random number between 1 and 100 (inclusive)

3d35826288240c6818571e3054c8aef516d3af0f
asmitasharma01 <asmita.sharma@my.jcu.edu.au>
2024-06-12 16:24:42 +0800

    add the functions

--- prac_04/1.lists_warmup.py
+++ prac_04/1.lists_warmup.py
@@ -4,7 +4,7 @@ def main():
     """
     numbers = [3, 1, 4, 1, 5, 9, 2]
 
-
+# Display the values of the expression
     print(numbers[0])
     print(numbers[-1])
     print(numbers[3])
@@ -15,16 +15,17 @@ def main():
     print("3" in numbers)
     print(numbers + [6, 5, 3])
 
-
+# Change the first element of number to "ten"
     numbers[0] = "ten"
 
-
+# Change the last elemet of number to 1
     numbers[-1] = 1
 
-
+# Print all elements from numbers except the first two
     print(numbers[2:])
 
 
+# Print whether 9 is an element of number
     print(9 in numbers)
 
 if __name__ == "__main__":
